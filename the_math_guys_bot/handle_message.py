614aa578ae80818ebf4ba55a5d4a8ef282aca877
MathItYT <benjamin.ubilla@uc.cl>
2024-01-10 13:50:36 -0300

    Fixed is_spam

--- the_math_guys_bot/handle_message.py
+++ the_math_guys_bot/handle_message.py
@@ -19,21 +19,29 @@ def is_spam(message_content: str, client: OpenAI) -> bool:
         frequency_penalty=0,
         presence_penalty=0,
     ).choices[0].message.content
-    return ("yes" in openai_response.lower()) or ("si" in openai_response.lower())
+    print(f"[is_spam] {message_content}: {openai_response}")
+    return ("yes" in openai_response.lower()) or ("si" in openai_response.lower()) or ("sí" in openai_response.lower())
 
 
 async def handle_message(message: Message, client: OpenAI):
     if is_spam(message.content, client):
         await message.channel.send(f"Este mensaje es posible spam. Ping a <@{MATHLIKE_USER_ID}> para que lo revise.")
+        return
     if "hola" in message.content.lower():
         await message.channel.send("¡Hola!")
+        return  
     if "adios" in message.content.lower():
         await message.channel.send("¡Que te vaya bien!")
+        return
     if "gracias" in message.content.lower():
         await message.channel.send("¡De nada!")
+        return
     if "buenos dias" in message.content.lower():
         await message.channel.send("¡Buenos días!")
+        return
     if "buenas tardes" in message.content.lower():
         await message.channel.send("¡Buenas tardes!")
+        return
     if "buenas noches" in message.content.lower():
         await message.channel.send("¡Buenas noches!")
+        return
