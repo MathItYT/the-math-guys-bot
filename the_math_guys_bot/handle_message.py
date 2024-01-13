eb9652c7441713466e7c2566aad2bc97897fd907
MathItYT <benjamin.ubilla@uc.cl>
2024-01-12 23:20:46 -0300

    Updated conversational feature

--- the_math_guys_bot/handle_message.py
+++ the_math_guys_bot/handle_message.py
@@ -9,24 +9,6 @@ BOT_USER_ID: Final[int] = 1194231765175369788
 
 
 async def handle_message(message: Message, client: OpenAI) -> None:
-    if "hola" in message.content.lower():
-        await message.channel.send("¡Hola!")
-        return  
-    if "adios" in message.content.lower():
-        await message.channel.send("¡Que te vaya bien!")
-        return
-    if "gracias" in message.content.lower():
-        await message.channel.send("¡De nada!")
-        return
-    if "buenos dias" in message.content.lower():
-        await message.channel.send("¡Buenos días!")
-        return
-    if "buenas tardes" in message.content.lower():
-        await message.channel.send("¡Buenas tardes!")
-        return
-    if "buenas noches" in message.content.lower():
-        await message.channel.send("¡Buenas noches!")
-        return
     if BOT_USER_ID in [user.id for user in message.mentions]:
         with open("messages.json", "r") as f:
             messages: list[dict[str, str]] = json.load(f)
@@ -43,5 +25,3 @@ async def handle_message(message: Message, client: OpenAI) -> None:
         print(f"[GPT-3] {message.author}: {response}")
         await message.channel.send(response)
         messages.append({"role": "assistant", "content": response})
-        with open("messages.json", "w") as f:
-            json.dump(messages, f)
