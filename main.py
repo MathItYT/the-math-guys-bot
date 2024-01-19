4fb5d9781dc9ecd44457862bdfbb65eb5df2cec0
MathItYT <benjamin.ubilla@uc.cl>
2024-01-18 21:30:42 -0300

    Fixed plotting

--- main.py
+++ main.py
@@ -176,6 +176,9 @@ def main():
     plt.rcParams["axes.facecolor"] = (0, 0, 0, 0)
     plt.rcParams["savefig.facecolor"] = (0, 0, 0, 0)
     plt.rcParams["axes.edgecolor"] = (1, 1, 1, 1)
+    plt.rcParams["axes.labelcolor"] = (1, 1, 1, 1)
+    plt.rcParams["figure.figsize"] = (2000/300, 2000/300)
+    plt.rcParams["figure.dpi"] = 300
     client.run(DISCORD_TOKEN)
 
 
