ebff75a1769f9627d2e835162da2dd85632f987f
MathItYT <benjamin.ubilla@uc.cl>
2024-01-22 01:22:58 -0300

    Fixed AI

--- the_math_guys_bot/handle_message.py
+++ the_math_guys_bot/handle_message.py
@@ -1,25 +1,44 @@
 from discord import Message
-from typing import Final
-from vertexai.preview.generative_models import GenerativeModel, Image
+from typing import Final, Any
+import google.generativeai as genai
+import os
+from PIL import Image
+from io import BytesIO
 
 
 MATHLIKE_USER_ID: Final[int] = 546393436668952663
 BOT_USER_ID: Final[int] = 1194231765175369788
-model = GenerativeModel("gemini-pro-vision")
-chat = model.start_chat()
+GOOGLE_API_KEY: Final[str] = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-pro-vision")
+messages: list[dict[str, Any]] = []
 
 
-async def get_images(message: Message) -> list[Image]:
-    images: list[Image] = []
+async def get_images(message: Message) -> list[Image.Image]:
+    images: list[Image.Image] = []
     if len(message.attachments) > 0:
         for attachment in message.attachments:
             if attachment.height is not None:
-                images.append(Image.from_bytes(await attachment.read()))
+                images.append(Image.open(BytesIO(await attachment.read())))
     return images
 
 
-def generate_response(message: str, images: list[Image]) -> str:
-    response = chat.send_message(images + [message])
+def free_memory() -> None:
+    global messages
+    if len(messages) >= 100:
+        del messages[0]
+
+
+def generate_response(message: str, images: list[Image.Image]) -> str:
+    global messages
+    messages.append({"role": "user", "parts": [message, *images]})
+    response = model.generate_content(messages)
+    try:
+        response.text
+    except AttributeError:
+        return "Ha ocurrido un error al generar la respuesta."
+    messages.append({"role": "model", "parts": [response.text]})
+    free_memory()
     return response.text
 
 
