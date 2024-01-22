2456acdd100f588ede578aae3a4f9e1589e5e0e9
MathItYT <benjamin.ubilla@uc.cl>
2024-01-22 01:57:39 -0300

    Fix AI

--- the_math_guys_bot/handle_message.py
+++ the_math_guys_bot/handle_message.py
@@ -1,41 +1,50 @@
 from discord import Message
-from typing import Final, Any
+from typing import Final, Optional
 import google.generativeai as genai
 import google.ai.generativelanguage as glm
+from pathlib import Path
 import os
-from PIL import Image
-from io import BytesIO
 
 
 MATHLIKE_USER_ID: Final[int] = 546393436668952663
 BOT_USER_ID: Final[int] = 1194231765175369788
 GOOGLE_API_KEY: Final[str] = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-pro-vision")
+model1 = genai.GenerativeModel("gemini-pro-vision")
+model2 = genai.GenerativeModel("gemini-pro")
 
 
-async def get_images(message: Message) -> list[Image.Image]:
-    images: list[Image.Image] = []
+async def save_image(message: Message) -> Optional[bytes]:
     for attachment in message.attachments:
         if attachment.content_type.startswith("image"):
-            images.append(Image.open(BytesIO(await attachment.read())))
-    return images
+            with open(f"temp{Path(attachment.filename).suffix}", "wb") as f:
+                await attachment.save(f)
+            return Path(f"temp{Path(attachment.filename).suffix}").read_bytes()
+    return None
 
-
-def generate_response(message: str, images: list[Image.Image]) -> str:
-    response = model.generate_content(
-        glm.Content(
-            parts=[
-                glm.Part(text=message),
-                glm.Part(
-                    inline_data=glm.Blob(
-                        mime_type="image/png",
-                        data=images[0].tobytes()
+def generate_response(message: str, image: Optional[bytes]) -> str:
+    if image is not None:
+        response = model1.generate_content(
+            glm.Content(
+                parts=[
+                    glm.Part(text=message),
+                    glm.Part(
+                        inline_data=glm.Blob(
+                            mime_type="image/png",
+                            data=image
+                        )
                     )
-                )
-            ]
+                ]
+            )
         )
-    )
+    else:
+        response = model2.generate_content(
+            glm.Content(
+                parts=[
+                    glm.Part(text=message)
+                ]
+            )
+        )
     try:
         response.text
     except AttributeError:
@@ -45,6 +54,6 @@ def generate_response(message: str, images: list[Image.Image]) -> str:
 
 async def handle_message(message: Message) -> None:
     if BOT_USER_ID in [user.id for user in message.mentions]:
-        images = await get_images(message)
-        response: str = generate_response(message.content.replace(f"<@{BOT_USER_ID}>", ""), images)
+        image = await save_image(message)
+        response: str = generate_response(message.content.replace(f"<@{BOT_USER_ID}>", ""), image)
         await message.channel.send(response)
