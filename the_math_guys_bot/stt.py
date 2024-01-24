c234fbe165924930a33723de71f6482742565921
MathItYT <benjamin.ubilla@uc.cl>
2024-01-24 00:38:54 -0300

    Fixed voice

--- the_math_guys_bot/stt.py
+++ the_math_guys_bot/stt.py
@@ -1,9 +1,10 @@
 from google.cloud import speech
+from io import BytesIO
 
 
-def speech_to_text(audio: bytes):
+def speech_to_text(audio: BytesIO):
     client = speech.SpeechClient()
-    audio = speech.RecognitionAudio(content=audio)
+    audio = speech.RecognitionAudio(content=audio.read())
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.MP3,
         language_code="es-ES",
