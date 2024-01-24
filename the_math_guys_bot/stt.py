6e9d288dbc7bdcb72487f0c3b6e0f5534750e6e3
MathItYT <benjamin.ubilla@uc.cl>
2024-01-24 01:26:02 -0300

    Fixed voice

--- the_math_guys_bot/stt.py
+++ the_math_guys_bot/stt.py
@@ -9,7 +9,7 @@ def speech_to_text(audio: BytesIO):
     bucket = storage.Client().bucket(bucket_name)
     blob = bucket.blob("audio.wav")
     blob.upload_from_file(audio)
-    uri = f"gs://{bucket_name}/audio.mp3"
+    uri = f"gs://{bucket_name}/audio.wav"
     audio = speech.RecognitionAudio(uri=uri)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
