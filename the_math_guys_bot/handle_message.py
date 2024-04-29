00807503cf2089e2ee1693aa3b26b2d4a1373f94
MathItYT <benjamin.ubilla@uc.cl>
2024-04-29 15:47:34 -0400

    Fixed some bugs

--- the_math_guys_bot/handle_message.py
+++ the_math_guys_bot/handle_message.py
@@ -4,8 +4,10 @@ import google.generativeai as genai
 import google.ai.generativelanguage as glm
 from pathlib import Path
 import os
-import requests
-import subprocess
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 CONTEXT: Final[str] = "Contexto: Tu nombre es TheMathGuysBot y eres un bot de Discord " \
@@ -13,32 +15,14 @@ CONTEXT: Final[str] = "Contexto: Tu nombre es TheMathGuysBot y eres un bot de Di
                       "bromas de los demás y cuando te piden hacer chistes, haces chistes." \
                       "Además, eres profesor de matemáticas y ayudas a los " \
                       "miembros del servidor con sus dudas matemáticas, de computación o física," \
-                      "excepto si es para una prueba, examen o tarea.\n" \
-                      "Cuando te envían una imagen, la analizas y respondes " \
-                      "de acuerdo a lo que ves en la imagen y a lo que te dicen en el mensaje. " \
-                      "Si te piden convertir una imagen a texto, lo haces. Si te piden " \
-                      "convertir imagen a código LaTeX o Markdown, incluyes el código " \
-                      "en la respuesta. Si te piden que resuelvas un problema matemático en una " \
-                      "imagen, lo haces, pero si ves que es un problema de una tarea o examen, " \
-                      "no lo resuelves, diciendo amigablemente que es una falta de ética, sin " \
-                      "pistas ni sugerencias."
-svg_image = requests.get("https://images.ctfassets.net/nrgyaltdicpt/5xfm4NybWckHL5ndpdnBRq/bc0b5e27044820ea5cdd11648a68f543/LL30Fig12r-plain.svg").text
-with open("demo_image.svg", "w") as f:
-    f.write(svg_image)
-subprocess.run([
-    "inkscape",
-    "--export-background-opacity=0",
-    "--export-width=1000",
-    "--export-type=png",
-    "demo_image.svg"
-])
-DEMO_IMAGE: Final[bytes] = Path("demo_image.png").read_bytes()
+                      "excepto si es para una prueba, examen o tarea."
 
 MATHLIKE_USER_ID: Final[int] = 546393436668952663
 BOT_USER_ID: Final[int] = 1194231765175369788
 GOOGLE_API_KEY: Final[str] = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-pro")
+model_image = genai.GenerativeModel("gemini-pro-vision")
 chat = model.start_chat(history=[
     {
         "role": "user",
@@ -143,36 +127,6 @@ chat = model.start_chat(history=[
     {
         "role": "user",
         "parts": [
-            glm.Part(text="¿Puedes convertirme esta imagen a LaTeX?"),
-            glm.Part(
-                inline_data=glm.Blob(
-                    mime_type="image/png",
-                    data=DEMO_IMAGE
-                )
-            )
-        ]
-    },
-    {
-        "role": "model",
-        "parts": [
-r"""```tex
-Subscripts in math mode are written as $a_b$ and superscripts are written as $a^b$. These can be combined and nested to write expressions such as
-
-\[ T^{i_1 i_2 \dots i_p}_{j_1 j_2 \dots j_q} = T(x^{i_1},\dots,x^{i_p},e_{j_1},\dots,e_{j_q}) \]
- 
-We write integrals using $\int$ and fractions using $\frac{a}{b}$. Limits are placed on integrals using superscripts and subscripts:
-
-\[ \int_0^1 \frac{dx}{e^x} =  \frac{e-1}{e} \]
-
-Lower case Greek letters are written as $\omega$ $\delta$ etc. while upper case Greek letters are written as $\Omega$ $\Delta$.
-
-Mathematical operators are prefixed with a backslash as $\sin(\beta)$, $\cos(\alpha)$, $\log(x)$ etc.
-```"""
-        ]
-    },
-    {
-        "role": "user",
-        "parts": [
             glm.Part(text="¡Gracias!")
         ]
     },
@@ -204,35 +158,39 @@ def to_markdown(text: str) -> str:
 async def save_image(message: Message) -> Optional[bytes]:
     for attachment in message.attachments:
         if attachment.content_type.startswith("image"):
-            with open(f"temp{Path(attachment.filename).suffix}", "wb") as f:
-                await attachment.save(f)
-            return Path(f"temp{Path(attachment.filename).suffix}").read_bytes()
-    return None
+            mime_type = attachment.content_type
+            image = await attachment.read()
+            return image, mime_type
+    return None, None
 
 
-def generate_response(message: str, image: Optional[bytes]) -> str:
-    parts = [
-        glm.Part(text=message)
-    ]
+def generate_response(message: str, image: Optional[bytes], mime_type: Optional[str]) -> str:
     if image:
-        parts.append(glm.Part(
-            inline_data=glm.Blob(
-                mime_type="image/png",
-                data=image
+        response = model_image.generate_content(
+            glm.Content(
+                parts = [
+                    glm.Part(text=message),
+                    glm.Part(
+                        inline_data=glm.Blob(
+                            mime_type=mime_type,
+                            data=image
+                        )
+                    ),
+                ],
             )
-        ))
-    response = chat.send_message({
-        "role": "user",
-        "parts": parts
-    })
+        )
+    else:
+        response = chat.send_message(message)
     try:
-        return to_markdown(response.parts[0].text)
-    except Exception:
+        return to_markdown(response.text)
+    except Exception as e:
+        print(e)
+        print(response.candidates[0].safety_ratings)
         return "Ha ocurrido un error al generar la respuesta."
 
 
 async def handle_message(message: Message) -> None:
     if BOT_USER_ID in [user.id for user in message.mentions]:
-        image = await save_image(message)
-        response: str = generate_response(message.content.replace(f"<@{BOT_USER_ID}>", ""), image)
+        image, mime_type = await save_image(message)
+        response: str = generate_response(message.content.replace(f"<@{BOT_USER_ID}>", ""), image, mime_type)
         await message.channel.send(response)
